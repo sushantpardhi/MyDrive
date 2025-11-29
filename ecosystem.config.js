005537246f7771ce242e04060e823a41629b3fd6
Sushant Pardhi <sushantpardhi186@gmail.com>
2025-11-29 23:55:05 +0530

    refactor: comment out backend configuration in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
-    {
-      name: "mydrive-backend",
-      cwd: "./server",
-      script: "index.js",
-      env: {
-        NODE_ENV: "production",
-        PORT: 8080,
-      },
-      instances: 1,
-      exec_mode: "fork",
-    },
+    // {
+    //   name: "mydrive-backend",
+    //   cwd: "./server",
+    //   script: "index.js",
+    //   env: {
+    //     NODE_ENV: "production",
+    //     PORT: 8080,
+    //   },
+    //   instances: 1,
+    //   exec_mode: "fork",
+    // },
     {
       name: "mydrive-frontend",
       script: "serve",
