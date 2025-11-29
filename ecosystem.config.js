1610c8da65e8814c534ca789a0d1f01e242adcfe
Sushant Pardhi <sushantpardhi186@gmail.com>
2025-11-30 00:42:47 +0530

    refactor: comment out frontend app configuration in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,15 +11,15 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
     },
-    {
-      name: "mydrive-frontend",
-      script: "serve",
-      args: "serve --name frontend -- -s build -l 3000 --single-spa --listen :contentReference[oaicite:2]{index=2}",
-      env: {
-        NODE_ENV: "production",
-      },
-      instances: 1,
-      exec_mode: "fork",
-    },
+    // {
+    //   name: "mydrive-frontend",
+    //   script: "serve",
+    //   args: "serve --name frontend -- -s build -l 3000 --single-spa --listen :contentReference[oaicite:2]{index=2}",
+    //   env: {
+    //     NODE_ENV: "production",
+    //   },
+    //   instances: 1,
+    //   exec_mode: "fork",
+    // },
   ],
 };
