e653985fbb4265f9bbae811d623790854846f23b
Sushant Pardhi <sushantpardhi186@gmail.com>
2025-11-29 22:45:51 +0530

    refactor: streamline application startup by consolidating backend and frontend processes into ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "mydrive-backend",
+      cwd: "./server",
+      script: "index.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 8080,
+      },
+      instances: 1,
+      exec_mode: "fork",
+    },
+    {
+      name: "mydrive-frontend",
+      script: "serve",
+      args: "-s client/build -l 3000",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      exec_mode: "fork",
+    },
+  ],
+};
