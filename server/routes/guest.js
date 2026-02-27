a2bf78115e37dff5cdd565a124fbd4f95d95842f
Sushant Pardhi <sushantpardhi186@gmail.com>
2026-02-28 00:30:00 +0530

    feat: Add client environment variables to production workflow and update cookie sameSite attribute to 'none' for production.

--- server/routes/guest.js
+++ server/routes/guest.js
@@ -417,7 +417,7 @@ router.post(
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
 
